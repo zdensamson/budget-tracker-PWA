06822f792a83271a8fe0c49c17f139e3ca9660d5
Deku <zachdsamson@gmail.com>
2021-10-22 20:31:03 -0700

    added manifest.json file

--- public/service-worker.js
+++ public/service-worker.js
@@ -17,7 +17,7 @@ const FILES_TO_CACHE = [
     '/icons/icon-192x192.png',
     '/icons/icon-384x384.png',
     '/icons/icon-512x512.png',
-   
+    '/manifest.json'
 ];
 
 // Install the service worker
